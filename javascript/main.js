27bff9edb42aff05a19e642554a8435aaf069abc
dennislu <dennislu@fashionguide.com.tw>
2016-02-18 20:10:07 +0800

    modify

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,47 @@
 $(document).ready(function() {
-  // FLip blocks
-  function randomFlip() {
+  var _min = 1,
+      _max = 75;
 
-    var $blocks = $('div');
-    var random = 1 + Math.floor(Math.random() * 107);
+  card_init(_min, _max);
+  $('#lottery').click(function(){
+    randomFlip(_min, _max);
+    show();
+  });
 
-    $('div:nth-child(' + random + ')').addClass('flip-it');
-
-    setInterval(function() {
-      // var random = 1 + Math.floor(Math.random() * 75);
-      // $blocks.removeClass('flip-it');
-      // $('div:nth-child(' + random + ')').addClass('flip-it');
-    }, 1250);
-
-  }
-
-  function deploy() {
-    randomFlip();
-  }
-
-  deploy();
+  $(document).keyup(function(event){
+    if (event.keyCode == 32) {
+      randomFlip(_min, _max);
+      return false;
+    }
+  });
 });
+
+function randomFlip(min, max) {
+  $('div.flip-it').addClass('fliped').removeClass('flip-it');
+  $('#lot_' + get_random($('div.lottery_card'))).addClass('flip-it');
+}
+
+function get_random(doms) {
+  var min = doms.first().text(),
+      max = doms.last().text(),
+      random_pool = [];
+
+  doms.each(function(i){
+    if ($(this).attr('class') == 'fliped') {
+      return true;
+    }
+    random_pool.push($(this).text());
+  });
+  return random_pool[Math.floor(Math.random() * random_pool.length)];
+}
+
+function card_init(min, max) {
+  for (i = max; i >= min; i--) {
+    $('h1').after($('<div></div>').text(i).attr('id', 'lot_' + i).addClass('lottery_card'));
+  }
+}
+
+function show() {
+  var current_number = $('div.flip-it').first().text();
+  $('#show').append($('<div></div>').text(current_number).addClass('show_card'));
+}
